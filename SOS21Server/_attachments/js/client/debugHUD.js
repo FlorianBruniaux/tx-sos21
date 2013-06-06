16f9416feae1f7891e340e650f13dcf4b189b1e0
steven salaun <steven.salaun@gmail.com>
2013-06-06 11:42:24 +0200

    update debugHUD

--- SOS21Server/_attachments/js/client/debugHUD.js
+++ SOS21Server/_attachments/js/client/debugHUD.js
@@ -1,20 +1,13 @@
-define(['lib/melon', 'client'], function(melon){
+define(['lib/melon'], function(melon){
     var DebugHUD = me.HUD_Item.extend({
         init: function(x, y) {
             // call the parent constructor
-            console.log(x+";"+y);
             this.parent(x, y);
             this.font = new me.Font("Arial", 16, "black", "left");
-            
         },
-        /*update: function(value){
-            this.value = value;
-        },*/
         draw: function(context, x, y) {
-            //context.clearRect ( 0 , 0, 300 , 100 );
             this.font.draw(context, this.value, this.pos.x, this.pos.y);
         }
-     
     });
     return DebugHUD;
 });
