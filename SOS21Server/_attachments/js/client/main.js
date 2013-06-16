1a165d9b5169f1bcb42c1e54ec255331fc9bcde7
Steven Salaun <steven.salaun@gmail.com>
2013-06-16 19:25:27 +0200

    fix client

--- SOS21Server/_attachments/js/client/main.js
+++ SOS21Server/_attachments/js/client/main.js
@@ -1,23 +1,6 @@
-define(['maps', 'entities'], function(maps, entities){
+define(['entities'], function(entities){
     var ressources = {"g_ressources": []};
     
-    if(maps.maps){
-        maps.maps.forEach(function(map){
-            map.tiles.forEach(function(tileset){ // ajout des ressources tiles
-            	ressources.g_ressources.push({name : tileset, type : "image", src : maps.tiles_folder+tileset+".png"});
-            });
-            map.bg.forEach(function(bg){ // ajout des ressources tiles
-                ressources.g_ressources.push({name : bg, type : "image", src : maps.bg_folder+bg+".png"});
-            });
-            ressources.g_ressources.push({name : map.name, type : "tmx", src : maps.maps_folder+map.name+".json"}); // ajout de la carte TMX
-        });
-    }
-    if(entities.entities){
-		entities.entities.forEach(function(entity){
-            ressources.g_ressources.push({name: entity.name, type: "image", src: entities.entities_folder+entity.name+".png"});
-		});
-    };
-    ressources.players = {"mainPlayer": {}, "otherPlayers": []};
 	ressources.objects = [
 		{
 			"_id": "451c56e6b20f4499486c59034d12f25f",
@@ -34,7 +17,6 @@ define(['maps', 'entities'], function(maps, entities){
 			"colRect": {"x": 5,"w": 35,"y": 65,"h":20}
 		}
 	];
-	
 	if(entities.objects){
 		entities.objects.forEach(function(obj){
 			ressources.g_ressources.push({name: obj.name, type: "image", src: entities.objects_folder+obj.name+".png"});
