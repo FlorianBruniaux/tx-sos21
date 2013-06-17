b982d517e5c76bed0377aa514609f3275946f621
Julien Calderan <julien.calderan@gmail.com>
2013-06-18 00:57:51 +0200

    maj du filtre sur les changements de map des joueurs

--- SOS21Server/filters/place.js
+++ SOS21Server/filters/place.js
@@ -1,6 +1,4 @@
 function(o, req) {
-
-  // ?
-  return req.query.place == o.place || req.query.place == o.previousPlace;
-
+  //joueur autre que le joueur principale && le joueur entre sur la map || le joueur sort de la map && le joueur est en train de changer de place
+  return ((o._id && o._id != req.query.param) && (o.place == req.query.place || o.previousPlace == req.query.place) && o.changingPlace==true);
 }
